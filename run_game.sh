fab27645622dafbdf632f830ebbde4614338e805
Sebastian Nilsson <sebbehebbe@gmail.com>
2019-01-18 15:02:32 +0100

    Linux version

--- run_game.sh
+++ run_game.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./halite --replay-directory replays/ --turn-limit 50 -vvv --width 32 --height 32 "python3 valter-vit.py" "python3 valter-vit-2.py"
+./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3 valtervit.py" "python3 valtervit.py"
